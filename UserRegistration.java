05687cbd60b986d08ce3430384beff4b191f88c1
drs317lakshami <devrajkumar317@gmail.com>
2022-12-01 23:12:25 +0530

    Added :  Validate First Name and last name which start with Cap and has minimum 3 char

--- UserRegistration.java
+++ UserRegistration.java
@@ -6,7 +6,16 @@ import java.util.regex.Pattern;
 public class UserRegistration {
 
     public static void main(String[] args) {
-    	Registration.validFirstName();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the options:\n1. Validate First Name\n2. Validate Last Name");
+        switch (scanner.nextInt()){
+            case 1:
+                Registration.validFirstName();
+                break;
+            case 2:
+                Registration.validLastName();
+                break;
+        }
     }
 }
 
@@ -26,5 +35,19 @@ public class UserRegistration {
             System.out.println("First name is valid");
         else
             System.out.println("First name is Invalid");
+    }
+
+    public static void validLastName() {
+        System.out.print("Enter the Last Name : ");
+        String name = scanner.next();
+        String regex = "^[A-Z]{1}[a-z]{2,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(name);
+        boolean result = matcher.matches();
+
+        if (result)
+            System.out.println("Last name is valid");
+        else
+            System.out.println("Last name is Invalid");
     }
 }
